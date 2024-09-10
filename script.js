5c55725235650a0732c6a553bfbacedae6373ad0
ferdynand ergy pramudani <ferdynandergypramudani11@gmail.com>
2024-09-10 16:53:11 +0700

    add navbar setion

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+// navbar fixed
+window.onscroll = () => {
+  const header = document.querySelector("header");
+  const absoluteNav = header.offsetTop;
+
+  if (window.pageYOffset > absoluteNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
+
 // hamburger
 const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
 });
